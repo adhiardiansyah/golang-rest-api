8939bea141420307eb08e22f97bb96f22feeef35
Adhi Ardiansyah <adhiardiansyah23@gmail.com>
2021-09-06 16:14:55 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,164 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/shopspring/decimal"
+)
+
+var db *gorm.DB
+var err error
+
+type Product struct {
+	ID    int             `json:"id"`
+	Code  string          `json:"code"`
+	Name  string          `json:"name"`
+	Price decimal.Decimal `json:"price" sql:"type:decimal(16,2)"`
+}
+
+type Result struct {
+	Code    int         `json:"code"`
+	Data    interface{} `json:"data"`
+	Message string      `json:"message"`
+}
+
+func main() {
+	db, err = gorm.Open("mysql", "root:@/go_rest_api?charset=utf8&parseTime=True")
+
+	if err != nil {
+		log.Println("Connection failed", err)
+	} else {
+		log.Println("Connection established")
+	}
+
+	db.AutoMigrate(&Product{})
+
+	handleRequest()
+}
+
+func handleRequest() {
+	log.Println("Start the development server at http://127.0.0.1:9999")
+
+	myRouter := mux.NewRouter().StrictSlash(true)
+
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/api/products", createProduct).Methods("POST")
+	myRouter.HandleFunc("/api/products", getProducts).Methods("GET")
+	myRouter.HandleFunc("/api/products/{id}", getProduct).Methods("GET")
+	myRouter.HandleFunc("/api/products/{id}", updateProduct).Methods("PUT")
+	myRouter.HandleFunc("/api/products/{id}", deleteProduct).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":9999", myRouter))
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome!")
+}
+
+func createProduct(w http.ResponseWriter, r *http.Request) {
+	payloads, _ := ioutil.ReadAll(r.Body)
+
+	var product Product
+	json.Unmarshal(payloads, &product)
+
+	db.Create(&product)
+
+	res := Result{Code: 200, Data: product, Message: "Success create product"}
+	result, err := json.Marshal(res)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("ContentType", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(result)
+}
+
+func getProducts(w http.ResponseWriter, r *http.Request) {
+	products := []Product{}
+
+	db.Find(&products)
+
+	res := Result{Code: 200, Data: products, Message: "Success get products"}
+	results, err := json.Marshal(res)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("ContentType", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(results)
+}
+
+func getProduct(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	productID := vars["id"]
+
+	var product Product
+	db.First(&product, productID)
+
+	res := Result{Code: 200, Data: product, Message: "Success get product"}
+	result, err := json.Marshal(res)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("ContentType", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(result)
+}
+
+func updateProduct(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	productID := vars["id"]
+
+	payloads, _ := ioutil.ReadAll(r.Body)
+
+	var productUpdates Product
+	json.Unmarshal(payloads, &productUpdates)
+
+	var product Product
+	db.First(&product, productID)
+	db.Model(&product).Updates(&productUpdates)
+
+	res := Result{Code: 200, Data: product, Message: "Success update product"}
+	result, err := json.Marshal(res)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("ContentType", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(result)
+}
+
+func deleteProduct(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	productID := vars["id"]
+
+	var product Product
+	db.First(&product, productID)
+	db.Delete(&product)
+
+	res := Result{Code: 200, Message: "Success delete product"}
+	result, err := json.Marshal(res)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("ContentType", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(result)
+}
